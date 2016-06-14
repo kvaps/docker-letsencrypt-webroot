0ddc3ba753590818c573df26990d937bd3afbb0f
Anton Ohorodnyk <anton@ohorodnyk.name>
2016-06-14 23:17:45 +0300

    Removed bad comments

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 FROM quay.io/letsencrypt/letsencrypt
-MAINTAINER kvaps <kvapss@gmail.com>
+MAINTAINER Anton Ohorodnyk <anton@ohorodnyk.name>
 
-RUN apt-get update
-RUN apt-get -y install docker.io
-RUN apt-get clean
-RUN rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+RUN apt-get update && \
+    apt-get install -y apt-transport-https ca-certificates && \
+    apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D && \
+    echo "deb https://apt.dockerproject.org/repo ubuntu-$(lsb_release -sc) main" > /etc/apt/sources.list.d/docker.list && \
+    apt-get update && \
+    apt-get -y install docker-engine && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
 ADD start.sh /bin/start.sh
 
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [ -z "$DOMAINS" ] ; then
   echo "No domains set, please fill -e 'DOMAINS=example.com www.example.com'"
@@ -15,101 +15,148 @@ if [ -z "$WEBROOT_PATH" ] ; then
   exit 1
 fi
 
-DARRAYS=(${DOMAINS})
+DOMAINS_ARRAY=(${DOMAINS})
 EMAIL_ADDRESS=${EMAIL}
-LE_DOMAINS=("${DARRAYS[*]/#/-d }")
 
 exp_limit="${EXP_LIMIT:-30}"
 check_freq="${CHECK_FREQ:-30}"
 
+SCALE_ENABLED="${SCALE_ENABLED:-disabled}"
+DEFAULT_CONTAINER_SCALE="${DEFAULT_CONTAINER_SCALE:-1}"
+
+
+container_id=$HOSTNAME
+container_name=$(docker inspect --format="{{.Name}}" ${container_id} | grep -oP '[0-9A-Za-z]+_[a-zA-Z0-9_.-]+_[0-9]+')
+network_name=$(docker inspect --format="{{.HostConfig.NetworkMode}}" ${container_id})
+image_name=$(echo ${container_name} | sed -r 's/_[0-9]+$//')
+scale_num=$(echo ${container_name} | grep -oP '[0-9+]$')
+
+echo "[NOTICE] Starting letsencrypt container $container_name($container_id)..."
+
+if [[ "$SCALE_ENABLED" == 'disabled' ]]; then
+    echo "[NOTICE] Scaling is disabled for letsencrypt"
+    if [[ "$DEFAULT_CONTAINER_SCALE" != $scale_num ]]; then
+        echo "[NOTICE] Stopping container with scale number $scale_num and container id $container_id"
+        docker stop ${container_id}
+    fi
+else
+    echo "[WARNING] Scaling is not disabled for letsencrypt. Starting container with scale number $scale_num..."
+fi
+
+dry_run="--dry-run"
+ENV_TYPE="${ENV_TYPE:0}"
+if [ "$ENV_TYPE" == "production" ]; then
+    echo "[NOTICE] Container started in production mode"
+    dry_run=""
+else
+    echo "[NOTICE] Container started in developer mode"
+fi
+
+env
+
 le_hook() {
-    all_links=($(env | grep -oP '^[0-9A-Z_-]+(?=_ENV_LE_RENEW_HOOK)'))
-    compose_links=($(env | grep -oP '^[0-9A-Z]+_[a-zA-Z0-9_.-]+_[0-9]+(?=_ENV_LE_RENEW_HOOK)'))
+    local network_containers_name=$(docker network inspect --format '{{range $index, $lement := .Containers}}{{.Name}} {{end}}' ${network_name})
     
-    except_links=($(
-        for link in ${compose_links[@]}; do
-            compose_project=$(echo $link | cut -f1 -d"_")
-            compose_name=$(echo $link | cut -f2- -d"_" | sed 's/_[^_]*$//g')
-            compose_instance=$(echo $link | grep -o '[^_]*$')
-            echo ${compose_name}_${compose_instance}
-            echo ${compose_name}
-        done
-    ))
-    
-    containers=($(
-        for link in ${all_links[@]}; do
-            [[ " ${except_links[@]} " =~ " ${link} " ]] || echo $link
-        done
-    ))
-    
-    for container in ${containers[@]}; do
-        command=$(eval echo \$${container}_ENV_LE_RENEW_HOOK)
-        command=$(echo $command | sed "s/@CONTAINER_NAME@/${container,,}/g")
-        echo "[INFO] Run: $command"
-        eval $command
+    echo "[NOTICE] Start checking containers from network $network_name"
+    for network_container_name in ${network_containers_name[@]}; do
+        echo "[NOTICE] Checking container $network_container_name"
+        if [[ ! "$network_container_name" =~ ^.*${image_name}_[0-9]+$ ]]; then
+            echo "[NOTICE] It's not letsencrypt container $network_container_name"
+            if [[ "$network_container_name" =~ ^.+_${scale_num}$ ]] || [[ "$SCALE_ENABLED" == 'disabled' ]]; then
+                echo "[NOTICE] Restarting container ${network_container_name}"
+                docker restart ${network_container_name}
+            fi
+        fi
     done
 }
 
 le_fixpermissions() {
     echo "[INFO] Fixing permissions"
-        chown -R ${CHOWN:-root:root} /etc/letsencrypt
-        find /etc/letsencrypt -type d -exec chmod 755 {} \;
-        find /etc/letsencrypt -type f -exec chmod ${CHMOD:-644} {} \;
+    chown -R ${CHOWN:-root:root} /etc/letsencrypt
+    find /etc/letsencrypt -type d -exec chmod 755 {} \;
+    find /etc/letsencrypt -type f -exec chmod ${CHMOD:-644} {} \;
+    echo "[INFO] Fixed permissions"
 }
 
 le_renew() {
-    certbot certonly --webroot --agree-tos --renew-by-default --email ${EMAIL_ADDRESS} -w ${WEBROOT_PATH} ${LE_DOMAINS}
+    local domains=$1
+    echo "[NOTICE] Getting new certificates"
+    certbot certonly --webroot --agree-tos --renew-by-default ${dry_run} --email ${EMAIL_ADDRESS} -w ${WEBROOT_PATH} -d ${domains}
+    echo "[NOTICE] Getted new certificates"
     le_fixpermissions
     le_hook
 }
 
 le_check() {
-    cert_file="/etc/letsencrypt/live/$DARRAYS/fullchain.pem"
+    local domains=$1
+    local first_domain="$( cut -d ',' -f 1 <<< "$domains" )"
+    
+    local cert_file="/etc/letsencrypt/live/$first_domain/fullchain.pem"
+    
+    echo "[NOTICE] Start processing domains $first_domain($domains)"
     
     if [ -f $cert_file ]; then
-    
-        exp=$(date -d "`openssl x509 -in $cert_file -text -noout|grep "Not After"|cut -c 25-`" +%s)
-        datenow=$(date -d "now" +%s)
-        days_exp=$[ ( $exp - $datenow ) / 86400 ]
         
-        echo "Checking expiration date for $DARRAYS..."
+        local exp=$(date -d "`openssl x509 -in $cert_file -text -noout|grep "Not After"|cut -c 25-`" +%s)
+        local cert_domains=$(openssl x509 -in $cert_file -text -noout | grep -oP '(?<=DNS:)[^,]*' | tr '\n' ' ')
+        local datenow=$(date -d "now" +%s)
+        local days_exp=$[ ( $exp - $datenow ) / 86400 ]
+        
+        local cert_domains_string=${cert_domains// /,}
+        
+        echo "Checking expiration date for $first_domain($cert_domains_string)..."
         
         if [ "$days_exp" -gt "$exp_limit" ] ; then
             echo "The certificate is up to date, no need for renewal ($days_exp days left)."
         else
-            echo "The certificate for $DARRAYS is about to expire soon. Starting webroot renewal script..."
-            le_renew
-            echo "Renewal process finished for domain $DARRAYS"
+            echo "The certificate for $first_domain($cert_domains_string) is about to expire soon. Starting webroot renewal script for domains $first_domain($domains)..."
+            le_renew $domains
+            echo "Renewal process finished for domain $first_domain($domains)"
         fi
-
-        echo "Checking domains for $DARRAYS..."
-
-        domains=($(openssl x509  -in $cert_file -text -noout | grep -oP '(?<=DNS:)[^,]*'))
-        new_domains=($(
-            for domain in ${DARRAYS[@]}; do
-                [[ " ${domains[@]} " =~ " ${domain} " ]] || echo $domain
+        
+        echo "Compairing domains for $first_domain($cert_domains_string) and $first_domain($domains)..."
+        
+        local domains_array=${domains//,/ }
+        local new_domains=($(
+            for domain in ${domains_array[@]}; do
+                [[ " ${cert_domains[@]} " =~ " ${domain} " ]] || echo $domain
             done
         ))
-
+        
+        new_domains_string=$(echo $new_domains | tr ' ' ',')
         if [ -z "$new_domains" ] ; then
             echo "The certificate have no changes, no need for renewal"
         else
-            echo "The list of domains for $DARRAYS certificate has been changed. Starting webroot renewal script..."
-            le_renew
-            echo "Renewal process finished for domain $DARRAYS"
+            echo "The list of domains for $first_domain($cert_domains_string) certificate has been changed to $first_domain($domains). List of new domains($new_domains_string). Starting webroot renewal script..."
+            le_renew $domains
+            echo "Renewal process finished for domains $first_domain($domains)"
         fi
-
-
+        
     else
-      echo "[INFO] certificate file not found for domain $DARRAYS. Starting webroot initial certificate request script..."
-      le_renew
-      echo "Certificate request process finished for domain $DARRAYS"
+        echo "Certificate for domain $first_domain not found. Starting webroot renewal script..."
+        le_renew $domains
+        echo "Renewal process finished for domains $first_domain($domains)"
     fi
-
-    if [ "$1" != "once" ]; then
-        sleep ${check_freq}d
-        le_check
-    fi
+    
+    echo "[NOTICE] Finished process domains $first_domain($domains)"
 }
 
-le_check $1
+le_check_domains() {
+    for domains in ${DOMAINS_ARRAY[@]}; do
+        le_check $domains
+    done
+}
+
+le_check_cycle() {
+    local arg_once=$1
+    while true; do
+        le_check_domains
+        if [ "$arg_once" == "once" ]; then
+            echo "[NOTICE] Container started in script mode. Stopping script..."
+            break
+        fi
+        sleep ${check_freq}d
+    done
+}
+
+le_check_cycle $1
